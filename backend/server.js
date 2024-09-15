1adfb062736d5ad83995d8fe04e8d3b167d815b5
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-09-15 22:52:21 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; 
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -23,6 +24,15 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
     connectDB()
 	console.log("Server is running on http://localhost:" + PORT);
